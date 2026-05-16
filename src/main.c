f93d41ebe7d955e9c81bb3c43547a8eb02b80fbd
Sqwea1 <sqwea111@mail.ru>
2026-05-16 22:23:07 +0000

    add program

--- src/main.c
+++ src/main.c
@@ -1,8 +1,44 @@
-/* hello.c */
+/*
+ * Author: Kot Aleksey
+ * Group: 12a
+ * Description: Lucky six-digit numbers (variant 10)
+ *              A number is lucky if sum of first three digits equals sum of last three.
+ */
+
 #include <stdio.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+// Function to check if a six-digit number is lucky
+int is_lucky(int num) {
+		int first_sum = 0, last_sum = 0;
+		int i;
+
+		// Extract digits: num is between 100000 and 999999
+		// Last three digits
+		for (i = 0; i < 3; i++) {
+				last_sum += num % 10;
+				num /= 10;
+		}
+		// First three digits
+		for (i = 0; i < 3; i++) {
+				first_sum += num % 10;
+				num /= 10;
+		}
+
+		return (first_sum == last_sum);
+}
+
+int main() {
+		int count = 0;
+		int number;
+
+		printf("Lucky six-digit numbers:\n");
+		for (number = 100000; number <= 999999; number++) {
+				if (is_lucky(number)) {
+						printf("%06d\n", number);
+						count++;
+				}
+		}
+
+		printf("\nTotal lucky numbers: %d\n", count);
+		return 0;
 }
